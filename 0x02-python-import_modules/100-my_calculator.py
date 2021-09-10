2c85e93fb8daa94fd562c72f860084661787c171
luffy981 <jhonsmith981@hotmail.com>
2021-09-09 22:09:18 -0500

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         if sys.argv[2] == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+            print("{:d} {} {:d} = {:d}".format(a, sys.argv[2], b, add(a, b)))
         elif sys.argv[2] == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+            print("{:d} {} {:d} = {:d}".format(a, sys.argv[2], b, sub(a, b)))
         elif sys.argv[2] == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+            print("{:d} {} {:d} = {:d}".format(a, sys.argv[2], b, mul(a, b)))
         elif sys.argv[2] == "*":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+            print("{:d} {} {:d} = {:d}".format(a, sys.argv[2], b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
