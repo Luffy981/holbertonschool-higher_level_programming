778af899519bc9c2e123a7c45ce778497bee35b5
luffy981 <jhonsmith981@hotmail.com>
2021-09-14 04:08:03 +0000

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -17,5 +17,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = (0, 0)
         else:
             tuple_b = tuple_b[0], 0
-    res = tuple(map(sum, zip(tuple_a, tuple_b)))
+    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return res
