01553f446168176dd3229d9efd4a9d55a2fef99e
luffy981 <jhonsmith981@hotmail.com>
2021-09-14 02:21:20 +0000

    Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
     print('\n'.join([''.join(['{:2}'.format(item) for item in row])
     for row in matrix]))
