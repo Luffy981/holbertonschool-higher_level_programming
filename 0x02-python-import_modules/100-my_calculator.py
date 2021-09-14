69bc0308ddc0836063021d176c50559bbff1342c
luffy981 <jhonsmith981@hotmail.com>
2021-09-14 20:55:13 +0000

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,12 +6,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        op = sys.argv[2]
-        f = {"+": add, "-": mul, "/": div}
-	if op not in f:
-	    print("Unknown operator, Available operators: +, -, * and /")
-	    exit(1)
-	print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    f = {"+": add, "-": mul, "/": div}
+    if op not in f:
+        print("Unknown operator, Available operators: +, -, * and /")
+        exit(1)
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
