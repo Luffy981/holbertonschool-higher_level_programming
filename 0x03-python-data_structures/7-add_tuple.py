162fafbc2a83bff802f183583a6b293611facff8
luffy981 <jhonsmith981@hotmail.com>
2021-09-14 03:37:17 +0000

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = len(tuple_a)
+    b = len(tuple_b)
+    if a < 2:
+        if a == 0:
+            tuple_a(0, 0)
+        else:
+            tuple_a = tuple_a[0], 0
+    if b < 2:
+        if b == 0:
+            tuple_b(0, 0)
+        else:
+            tuple_b = tuple_b[0], 0
+    res = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    return res
