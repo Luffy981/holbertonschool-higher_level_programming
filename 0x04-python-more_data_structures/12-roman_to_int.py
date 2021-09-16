742de1c636e5644957cdaaab55b6b84e99c72e0a
luffy981 <jhonsmith981@hotmail.com>
2021-09-15 22:37:49 -0500

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
-    if len(roman_string) == 0:
+    if not bool(roman_string):
         return None
     check = isinstance(roman_string, str)
     if check is False:
