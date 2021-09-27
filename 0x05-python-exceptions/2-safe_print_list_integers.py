3d32f9b17f43c145505decb64d4135901d216031
luffy981 <jhonsmith981@hotmail.com>
2021-09-27 16:29:06 -0500

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in  my_list[0:x]:
+        try:
+            print("{:d}".format(i), end="")
+            count += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+    print()
+    return count
