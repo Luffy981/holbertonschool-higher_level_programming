cd0fdcabe86fb773ba758f709a614ffbbf96bf5b
luffy981 <jhonsmith981@hotmail.com>
2021-09-27 16:30:57 -0500

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in  my_list[0:x]:
+    for i in my_list[0:x]:
         try:
             print("{:d}".format(i), end="")
             count += 1
