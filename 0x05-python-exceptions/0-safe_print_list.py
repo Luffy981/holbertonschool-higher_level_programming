4ad79b78efbd575acf2a0a304b60f930e7d5519d
luffy981 <jhonsmith981@hotmail.com>
2021-09-27 15:00:48 -0500

    task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in my_list:
-        try:
-            if count < x or count > len(my_list):
-                print("{:d}".format(int(i)), end="")
-            else:
-                break
-        except ValueError:
-            break
-        finally:
-            print("{:d}".format(count))
-        count += 1
+    try:
+        print(''.join(map(str, my_list[0:x]))) 
+        for i in my_list:
+            count += 1
+    except ValueError:
+        pass
+    finally:
+        if x > count:
+            return count
+        else:
+            return x
 
