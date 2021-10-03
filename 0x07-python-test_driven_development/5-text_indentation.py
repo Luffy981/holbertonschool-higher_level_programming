01dc9603f38647eab46dd3d70a9173429b2b4d9c
luffy981 <jhonsmith981@hotmail.com>
2021-10-03 17:42:36 -0500

    Text identation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+def text_indentation(text):
+    """function that prints a text with 2 new lines after
+    each of these characters: ., ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    my_list = list(text)
+    for i in range(len(my_list)):
+        if my_list[i] == "." or my_list[i] == "?" or my_list[i] == ":":
+            if my_list[i + 1] and my_list[i + 2]:
+                my_list[i + 1] = "\n"
+                my_list.insert(i + 2, "\n")
+        i += 2
+    my_list = "".join(my_list)
+    print(my_list, end="")
