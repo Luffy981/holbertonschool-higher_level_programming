c0cf5b6c45435f2adf82afea7b3610510c1bf241
luffy981 <jhonsmith981@hotmail.com>
2021-10-03 23:09:47 -0500

    Max integer

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max(self):
+        """Max value into matrix with integer numbers"""
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([5, 10, 3, 89]), 89)
+
+    def test_types(self):
+        """Make sure type errors are raised when necessary"""
+        self.assertRaises(TypeError, max_integer, [1, 2, "Luffy", 4])
+        self.assertRaises(TypeError, max_integer, [1, False, True, 4])
+        self.assertRaises(TypeError, max_integer, [1, 2, ["Ola k ace"], 4])
+        self.assertRaises(TypeError, max_integer, "Monkey D. Luffy")
+
