6caf1af6af721c359d8eef3bbd0226671bc7b3b0
luffy981 <jhonsmith981@hotmail.com>
2021-10-04 15:29:00 -0500

    Area and permiter of rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """class rectangle
+    - area
+    - perimeter
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, new_width):
+        if type(new_width) is not int:
+            raise TypeError("width must be an integer")
+        if new_width < 0:
+            raise ValueError("width must be >=0")
+        self.__width = new_width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, new_height):
+        if type(new_height) is not int:
+            raise TypeError("height must be an integer")
+        if new_height < 0:
+            raise ValueError("height must be >=0")
+        self.__height = new_height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
