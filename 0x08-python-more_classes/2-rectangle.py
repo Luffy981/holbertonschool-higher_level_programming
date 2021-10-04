a1f45d3ccf69b1287a30bde8079fda8d289acd60
luffy981 <jhonsmith981@hotmail.com>
2021-10-04 15:32:30 -0500

    area and perimeter of rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         return self.__width
