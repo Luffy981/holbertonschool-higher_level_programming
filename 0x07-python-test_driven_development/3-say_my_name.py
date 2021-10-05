8b650271fa5591f166bd60b44225de58bd468345
luffy981 <jhonsmith981@hotmail.com>
2021-10-05 03:12:20 -0500

    say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+function say_my_name
+first_name
+last_name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    class say_my_name
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
