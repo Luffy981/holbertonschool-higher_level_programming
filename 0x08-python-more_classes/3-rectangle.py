f5a4b991fbaa7de791fb03bf1aaba965bc864ba5
luffy981 <jhonsmith981@hotmail.com>
2021-10-04 22:29:06 -0500

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
     def __str__(self):
         """Print square to stdout"""
         result = ""
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return result
         else:
             result += '\n'.join("#" * self.__width
