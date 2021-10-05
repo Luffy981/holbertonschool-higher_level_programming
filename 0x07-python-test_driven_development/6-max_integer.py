03c26f1e556e484a2f1baa8fe3264b08e5e2587e
luffy981 <jhonsmith981@hotmail.com>
2021-10-05 03:19:53 -0500

    max integer into list

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""
+Module to find the
+max integer
+in a list
 """
 
 
 def max_integer(lista=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
+    """
+    Function to find and return the max integer in a list of integers
+    If the list is empty, the function returns None
     """
     if len(lista) == 0:
         return None
