7b9686c16f151c3aac6eda6869dde78f5c3127dc
luffy981 <jhonsmith981@hotmail.com>
2021-10-05 14:34:58 -0500

    Multiply

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+function to multiply matrix
+parameters
+matrix a, matrix b
+"""
+
+
 def matrix_mul(m_a, m_b):
+    """
+    multipy matrix
+    """
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
@@ -12,8 +22,14 @@ def matrix_mul(m_a, m_b):
             raise TypeError("m_b must be a list of lists")
     if not m_a:
         raise ValueError("m_a can't be empty")
+    elif len(m_a) == 1:
+        if not m_a[0]:
+            raise ValueError("m_a can't be empty")
     if not m_b:
         raise ValueError("m_b can't be empty")
+    elif len(m_b) == 1:
+        if not m_b[0]:
+            raise ValueError("m_b can't be empty")
     message = "m_a should contain only integers or floats"
     message2 = "m_b should contain only integers or floats"
     if type(m_a[0]) is not list:
@@ -25,6 +41,7 @@ def matrix_mul(m_a, m_b):
         for row in m_a:
             if len_r != len(row):
                 raise TypeError("each row of m_a must be of the same size")
+            len_r = len(row)
             for item in row:
                 if type(item) not in [int, float]:
                     raise TypeError(message)
@@ -34,8 +51,8 @@ def matrix_mul(m_a, m_b):
             if type(j) not in [int, float]:
                 raise TypeError(message2)
     else:
-        len_r = len(m_a[0])
-        for row2 in m_a:
+        len_r = len(m_b[0])
+        for row2 in m_b:
             if len_r != len(row2):
                 raise TypeError("each row of m_b must be of the same size")
             len_r = len(row2)
@@ -48,7 +65,5 @@ def matrix_mul(m_a, m_b):
 #           result[i][j] += m_a[i][k] * m_b[k][j]
 #   result = [[sum(a * b for a, b in zip(m_a_row, m_b_column))
 #   for m_b_column in zip(*m_b)] for m_a_row in m_a]
-    for m_b_column in zip(*m_b):
-        for m_a_row in m_a:
-            result = sum(a * b for a, b in zip(m_a_row, m_b_column))
-    return result
+    res = [[sum(a * b for a, b in zip(r, c)) for c in zip(*m_b)] for r in m_a]
+    return res
