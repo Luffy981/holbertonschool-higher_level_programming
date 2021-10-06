fd16d92678998626b1732044b697387af2a12573
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 01:05:04 -0500

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import doctest
 """
 function to add 2 numbers
 numers a, b
@@ -22,7 +21,3 @@ def add_integer(a, b=98):
     a = int(a)
     b = int(b)
     return a + b
-
-
-if __name__ == '__main__':
-    doctest.testfile('0-add_integer.txt')
