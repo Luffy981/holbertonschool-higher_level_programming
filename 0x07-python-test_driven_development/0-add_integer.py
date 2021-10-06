281a65a18b3a8ebfb91fb53b37c81c3a2ef4971a
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 01:14:11 -0500

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,8 +7,12 @@ return a + b
 
 
 def add_integer(a, b=98):
-    """
-    Function to add 2 integers
+    """Return the addition of two numbers with spcific validation
+    Arguments:
+        a (int or float): first value to add
+        b (int or float): second value to add
+    Return:
+        the addition of a and b
     """
     if type(a) is not int and type(a) is not float:
         """ raise a TypeError if a isn't a integer or float'"""
@@ -16,8 +20,6 @@ def add_integer(a, b=98):
     elif type(b) is not int and type(b) is not float:
         """ raise a TypeError if a isn't a integer or float'"""
         raise TypeError("b must be an integer")
-    if not a:
-        raise ValueError(" fucntion need 2 arguments")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
