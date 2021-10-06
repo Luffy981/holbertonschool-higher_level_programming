715255582f2cc2a9f4c458f7d7cf177688b62ba5
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 09:45:26 -0500

    add integer fixed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,6 +17,9 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+    result a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
     if type(a) is float:
         a = int(a)
     if type(b) is float:
