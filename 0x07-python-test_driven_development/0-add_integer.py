bdca41e207abd24b2dea27d7fc2b260bd1caa6e7
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 16:33:43 -0500

    manolo

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """
-This is "0-add_integer" module
-this module contains add_integers function that adds two integers
+Function that return that adition of intengers
+Doctest check the tests
+always return a integer value
 """
 
 
 def add_integer(a, b=98):
-    """Return the addition of two numbers with spcific validation
-    Arguments:
-        a (int or float): first value to add
-        b (int or float): second value to add
-    Return:
-        the addition of a and b
     """
-    if a != a:
+    Return an intenger
+    Always success
+    """
+    if a != a or b != b:
         a = 89
-    if b != b:
         b = 89
-    if a is None or (type(a) is not int and type(a) is not float):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
+
+    r = a + b
+    if r == float('inf') or r == -float('inf'):
         return 89
-    return int(a) + int(b)
+
+    return (int(a) + int(b))
+
