1581e6523bf759ccae709b4d4a639a28631ef896
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 01:12:34 -0500

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,6 +18,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     if not a:
         raise ValueError(" fucntion need 2 arguments")
-    a = int(a)
-    b = int(b)
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
     return a + b
