df5bcd85ff98ff8f0acdb95803ff2dfcf5582372
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 19:57:09 -0500

    welcome back

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,16 +7,20 @@ Return: a + b
 
 
 def add_integer(a, b=98):
-    """Return adition of two numbers"""
+    """
+    function return addition
+    """
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    result = a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
+    return int(a) + int(b)
 
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
 
-    return a + b
