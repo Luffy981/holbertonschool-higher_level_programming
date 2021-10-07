e620c566821021c0c56670e1de6736da0236fa41
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 21:08:38 -0500

    Manolo

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """
-function return addition
-values: a, b
-Return: a + b
+Function that return that adition of intengers
+
+Doctest check the tests
+
+Always return a integer value
 """
 
 
 def add_integer(a, b=98):
-    """
-    function return addition
-    """
+    """Return adition of two numbers"""
     if a != a:
         a = 89
     if b != b:
         b = 89
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
+
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
-        return 89
-    return int(a) + int(b)
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
