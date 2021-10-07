2fd545c03c0e1bad5c3f5847bcc0cc8ebe70ebde
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 20:12:32 -0500

    fixed text identation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,12 +9,15 @@ indent
 def text_indentation(text):
     """function that prints a text with 2 new lines after
     each of these characters: ., ? and :"""
+    mee = "text_indentation() missing 1 required positional argument: 'text'"
+    if not text:
+        raise TypeError(mee)
     if type(text) is not str:
         raise TypeError("text must be a string")
     my_list = list(text)
     for i in range(len(my_list)):
         if my_list[i] == "." or my_list[i] == "?" or my_list[i] == ":":
-            if my_list[i + 1] and my_list[i + 2]:
+           if my_list[i + 1] and my_list[i + 2]:
                 my_list[i + 1] = "\n"
                 my_list.insert(i + 2, "\n")
         i += 2
