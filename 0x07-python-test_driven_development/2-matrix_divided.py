786be207868f7d0e3a9bf1bf64e727275c076635
luffy981 <jhonsmith981@hotmail.com>
2021-10-06 22:30:11 -0500

    shanny

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 """
-function matrixd divide
-parameters: matrix, div
-new  matrix
+function matrix_divided
+parametres: matrix, div
+Return: new matrix
 """
 
 
 def matrix_divided(matrix, div):
     """
-    function that divides all elements of a matrix
+    function use matrix and div
     """
-#    formatted_list = []
-#    for i in new_list:
-#        for item in i:
-#            formatted_list.append("%.2f" % item)
-#    new_list = [round(x, 2) for i in new_list for x in i]
     message = "matrix must be a matrix (list of lists) of integers/floats"
-    message2 = "Each row of the matrix must have the same size"
+    message1 = "Each row of the matrix must have the same size"
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
@@ -28,16 +23,16 @@ def matrix_divided(matrix, div):
         for item in matrix:
             if type(item) not in [int, float]:
                 raise TypeError(message)
-        ne = list(map(lambda item: round(item/div, 2), matrix))
+        my_list = list(map(lambda item: round(item/div, 2), matrix))
     else:
         for row in matrix:
             if i >= 1:
                 if len(matrix[i]) != len(matrix[i - 1]):
-                    raise TypeError(message2)
+                    raise TypeError(message1)
             for item in row:
                 if type(item) not in [int, float]:
                     raise TypeError(message)
             i += 1
-        m = matrix.copy()
-        ne = list(map(lambda r: list(map(lambda i: round(i/div, 2), r)), m))
-    return ne
+        my = matrix.copy()
+        re = list(map(lambda r: list(map(lambda i: round(i/div, 2), r)), my))
+    return re
