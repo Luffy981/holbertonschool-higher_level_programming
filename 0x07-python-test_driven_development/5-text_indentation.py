8822a6cb36ed624345587915f4e86ff17c44de53
luffy981 <jhonsmith981@hotmail.com>
2021-10-08 00:02:35 -0500

    Optimixed text identation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -16,18 +16,25 @@ def text_indentation(text):
     """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    flag = 0
-    text = text.strip()
-    for i in text:
-        if flag == 0:
-            if i == ' ':
-                continue
-            else:
-                flag = 1
-        if flag is 1:
-            if i == '.' or i == '?' or i == ':':
-                print(i)
-                print()
-                flag = 0
-            else:
-                print(i, end='')
+#    flag = 0
+#    text = text.strip()
+#    for i in text:
+#        if flag == 0:
+#            if i == ' ':
+#                continue
+#            else:
+#                flag = 1
+#        if flag is 1:
+#            if i == '.' or i == '?' or i == ':':
+#                print(i)
+#                print()
+#                flag = 0
+#            else:
+#                print(i, end='')
+    list_lines = []
+    for char in ".:?":
+        text = text.replace(char, char + "\n\n")
+    for lines in text.split('\n'):
+        list_lines.append(lines.strip(" "))
+    revised = "\n".join(list_lines)
+    print(revised, end="")
