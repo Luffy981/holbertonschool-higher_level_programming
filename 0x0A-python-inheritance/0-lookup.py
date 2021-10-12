bb9a0eead547f6884d7d4889aa9220585801bc59
luffy981 <jhonsmith981@hotmail.com>
2021-10-11 19:59:10 -0500

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,7 +4,7 @@
 
 def lookup(obj):
     """
-    function that returns the list of available 
+    function that returns the list of available
     attributes and methods of an object
     """
     return dir(obj)
