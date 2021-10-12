1dce993fe841c17403bc9ead849fc6254e72c56e
luffy981 <jhonsmith981@hotmail.com>
2021-10-11 21:50:07 -0500

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class square inherent from rectangle"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """class square"""
+    def __init__(self, size):
+        """initialize method"""
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """ area of rectangle"""
+        return self.__size * self.__size
+
