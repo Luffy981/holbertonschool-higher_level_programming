dbda18732d977d5bc4eaf940117f13547d788077
luffy981 <jhonsmith981@hotmail.com>
2021-10-11 21:30:17 -0500

    rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
-"""Cass definition"""
-
-
-class BaseGeometry:
-    """improve geometry"""
-    def area(self):
-        """Area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ function"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
 """Class rectangle inherent"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
