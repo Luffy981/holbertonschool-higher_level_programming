c50109cd1394c7846bfe85f3094028850e3ea6fc
luffy981 <jhonsmith981@hotmail.com>
2021-11-16 23:47:45 -0500

    fixed

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
--- number of records with the same score in the table second_table of the database hbtn_0c_0
--- the number of records for this score with the label number
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
+-- script that lists the number of records with the same score in the table
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
