5439e4b21291eb7543702eee3d81486ea4e7ae7d
luffy981 <jhonsmith981@hotmail.com>
2021-11-17 21:45:40 -0500

    Genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT tvs.title, tvsg.genre_id FROM tv_shows AS tvs LEFT JOIN tv_show_genres AS tvsg WHERE tvs.id=tvsg.show_id ORDER BY tvs.title, tvsg.genre_id ASC;
