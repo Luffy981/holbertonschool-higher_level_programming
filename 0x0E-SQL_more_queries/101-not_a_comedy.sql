d549c5c0239db5ad898489816a7da7b7888b7ab5
luffy981 <jhonsmith981@hotmail.com>
2021-11-18 00:34:13 -0500

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,9 +4,9 @@ LEFT JOIN
 (
 	SELECT ts.id FROM tv_shows AS ts
 	JOIN tv_show_genres AS tsg
-	ON ts.id=tsg.show_id
+		ON ts.id=tsg.show_id
 	JOIN tv_genres AS tg
-	ON tsg.genre_id=tg.id
+		ON tsg.genre_id=tg.id
 	WHERE tg.name = "Comedy"
 ) AS subselect
 ON subselect.id = ts.id WHERE subselect.id IS NULL ORDER BY ts.title ASC;
