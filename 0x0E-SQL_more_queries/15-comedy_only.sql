5ba7e700ac3e555af74ad62b330f64cb32204cb0
luffy981 <jhonsmith981@hotmail.com>
2021-11-17 22:31:06 -0500

    Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- script that lists all Comedy shows in the database hbtn_0d_tvshowsl
+SELECT ts.title FROM tv_shows  AS ts JOIN tv_show_genres tn ON ts.id=tn.show_id JOIN tv_genres AS tg ON tn.show_id=tg.id WHERE tg.name="Comedy" ORDER BY ts.title ASC;
