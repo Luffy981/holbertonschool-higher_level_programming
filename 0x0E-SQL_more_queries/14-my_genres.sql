f2fd56ec9e764210445cd3d285bc30b47726928c
luffy981 <jhonsmith981@hotmail.com>
2021-11-17 22:11:54 -0500

    My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,2 @@
+-- hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT tg.name, t.title FROM tv_genres  AS tg JOIN tv_show_genres tn ON tg.id=tn.genre_id JOIN tv_shows AS ts ON tn.show_id=ts.id WHERE ts.title="Dexter" ORDER BY tg.name ASC;
