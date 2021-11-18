5432459a224e0a9640f0473610fdd59ee8955aff
luffy981 <jhonsmith981@hotmail.com>
2021-11-17 23:25:19 -0500

    Not my genre

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT tg.name FROM tv_genres AS tg LEFT JOIN
+(
+	SELECT tg.id FROM tv_genres AS tg
+	JOIN tv_show_genres AS tsg ON tg.id=tsg.genre_id
+	JOIN tv_shows AS ts ON tg.show_id=ts.id
+	WHERE ts.title="Dexter")
+	ORDER BY tg.name ASC
+) AS subselect ON subselect.id=tg.id
+WHERE subselect.id IS NULL ORDER BY tg.name ASC;
