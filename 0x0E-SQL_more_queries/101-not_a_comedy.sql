c4d3bf2dbeee87ab86335fe77b932985a4b55cc2
luffy981 <jhonsmith981@hotmail.com>
2021-11-18 00:37:10 -0500

    No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,12 +1,12 @@
 -- ists all shows without the genre Comedy in the database hbtn_0d_tvshows
-SELECT ts.title FROM tv_shows AS ts
+SELECT sh.title FROM tv_shows AS sh
 LEFT JOIN
 (
-	SELECT ts.id FROM tv_shows AS ts
-	JOIN tv_show_genres AS tsg
-		ON ts.id=tsg.show_id
-	JOIN tv_genres AS tg
-		ON tsg.genre_id=tg.id
-	WHERE tg.name = "Comedy"
-) AS subselect
-ON subselect.id = ts.id WHERE subselect.id IS NULL ORDER BY ts.title ASC;
+       SELECT sh.id FROM tv_shows AS sh
+       JOIN tv_show_genres AS shgr
+       ON sh.id=shgr.show_id
+       JOIN tv_genres AS gr
+       ON shgr.genre_id=gr.id
+       WHERE gr.name = "Comedy"
+) AS subcomd
+ON subcomd.id = sh.id WHERE subcomd.id IS NULL ORDER BY sh.title ASC;
