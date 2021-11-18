accaf4589c0abca8ff14c1be9bd5820fe41ff282
luffy981 <jhonsmith981@hotmail.com>
2021-11-18 00:42:22 -0500

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,2 +1,2 @@
 -- ists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT ts.title SUM(tsr.rate) AS rating FROM tv_shows AS ts JOIN tv_show_ratings AS tsr ON ts.id=tsr.show_id GROUP BY ts.title ORDER BY rating DESC;
+SELECT ts.title, SUM(tsr.rate) AS rating FROM tv_shows AS ts JOIN tv_show_ratings AS tsr ON ts.id=tsr.show_id GROUP BY ts.title ORDER BY rating DESC;
