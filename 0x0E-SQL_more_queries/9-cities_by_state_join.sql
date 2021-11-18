9bbd2906d516219a2ccd8e09eb3a4ea3688ae794
luffy981 <jhonsmith981@hotmail.com>
2021-11-17 21:22:44 -0500

    try

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s WHERE s.id = c.id ORDER BY c.id ASC;
+SELECT c.id, c.name, s.name FROM cities AS c INNER JOIN states AS s WHERE s.state_id = c.id ORDER BY c.id ASC;
