224d52eaa63882139a4770585cd46dceddc6b87f
luffy981 <jhonsmith981@hotmail.com>
2021-11-17 22:44:05 -0500

    List shows and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT ts.tittle, tg.name FROM tv_shows AS ts LEFT JOIN tv_show_genres AS tsg ON ts.id=tsg.show_id LEFT JOIN tv_genres AS tg ON tsg.genre_id=tg.id ORDER BY ts.title, tg.name ASC;
