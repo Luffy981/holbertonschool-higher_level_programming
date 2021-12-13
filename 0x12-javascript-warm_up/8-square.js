7770f8564be7001d3c787f6afe1889e6b614d2ce
luffy981 <jhonsmith981@hotmail.com>
2021-12-13 18:05:16 -0500

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+for (let i = 0; i < number; i++) {
+  let concat = '';
+  for (let j = 0; j < number; j++) {
+    concat += 'x';
+  }
+  console.log(concat);
+}
