ef77d3fdf47147bdf905166bfc50d33a8b3b421b
luffy981 <jhonsmith981@hotmail.com>
2021-12-13 23:36:27 -0500

    fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,10 @@ if (!argv[3]) {
   console.log(0);
 } else if (argv[2] && argv[3]) {
   const unique = [...new Set(argv)];
-  unique.sort();
+  function comparar (a, b) {
+    return (a - b);
+  }
+  unique.sort(comparar);
   const number = unique.length;
   console.log(parseInt(unique[number - 2]));
 }
