20967be8c7df045db4d829e81d8020f459c49103
luffy981 <jhonsmith981@hotmail.com>
2021-12-14 12:49:03 -0500

    occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const i in list) {
+    if (searchElement === list[i]) {
+      count += 1;
+    }
+  }
+  return (count);
+};
