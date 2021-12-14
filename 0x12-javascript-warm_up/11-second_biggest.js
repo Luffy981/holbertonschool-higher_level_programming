67e76add72c110555d2988bfa7386a22d95029d5
luffy981 <jhonsmith981@hotmail.com>
2021-12-13 23:23:09 -0500

    fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,8 @@
 const { argv } = require('process');
 if (!argv[3]) {
   console.log(0);
+} else if (argv[2] && argv[3] && argv[2] === argv[3]) {
+  console.log(argv[2]);
 } else if (argv[2] && argv[3]) {
   const unique = [...new Set(argv)];
   unique.sort();
