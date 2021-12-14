f179a253c3a3752ea4e331e9fc53e9e4ccc956e2
luffy981 <jhonsmith981@hotmail.com>
2021-12-13 20:12:21 -0500

    biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('process');
-argv.sort();
-const number = argv.length;
-console.log(argv[number - 2]);
+if (!argv[3]) {
+  console.log(0);
+} else if (argv[2] && argv[3]) {
+  argv.sort();
+  const number = argv.length;
+  console.log(argv[number - 2]);
+}
+
