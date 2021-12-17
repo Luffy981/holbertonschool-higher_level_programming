92b40cb3f2fbdc774166cfe705d5d93776a3cb2e
luffy981 <jhonsmith981@hotmail.com>
2021-12-17 14:17:57 -0500

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+
+def SQLconnect():
+    try:
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], password=argv[2],
+                                        db=argv[3], charset="utf8")
+    except:
+        print("Can't connect to database")
+        return 0
+    print("Connected...")
+    cur = db_connection.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{name}' ORDER BY id ASC;"
+                .format(name=argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db_connection.close()
+
+
+SQLconnect()
