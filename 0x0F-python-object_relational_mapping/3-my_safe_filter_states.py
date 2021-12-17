74c9fc0f0e82b44274a0b3a024e365c7960fc0b0
luffy981 <jhonsmith981@hotmail.com>
2021-12-17 15:48:57 -0500

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+def sqlConection():
+    try:
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], password=argv[2],
+                                        db=argv[3], charset="utf8")
+    except:
+        print("Can't connect to database")
+        return 0
+    print("Connected...")
+    cur = db_connection.cursor()
+    sql = "SELECT * FROM states WHERE name = %s ORDER BY id ASC;"
+    data = (argv[4],)
+    cur.execute(sql, data)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db_connection.close()
+
+
+sqlConection()
