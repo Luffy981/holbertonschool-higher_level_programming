a7103176b4035dff77375485fcac6f6bda5da615
luffy981 <jhonsmith981@hotmail.com>
2021-12-17 19:44:30 -0500

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     Session.configure(bind=engine)
     session = Session()
     for id, name in session.query(State.id, State.name):
-        print(id, name)
+        print("{}: {}".format(id, name))
