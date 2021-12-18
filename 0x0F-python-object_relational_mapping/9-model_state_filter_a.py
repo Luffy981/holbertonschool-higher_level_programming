a732822392881478f75a6d2f5c73b85b6629f8ce
luffy981 <jhonsmith981@hotmail.com>
2021-12-18 13:16:48 -0500

    Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Initialize"""
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -6,6 +7,9 @@ from sys import argv
 
 
 if __name__ == "__main__":
+    """
+    Conecting database and queering
+    """
     sql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(sql.format(argv[1],
                                       argv[2], argv[3]), pool_pre_ping=True)
@@ -17,6 +21,6 @@ if __name__ == "__main__":
         for id, name in session.query(State.id, State.name).order_by(State.id):
             if 'a' in name:
                 print("{}: {}".format(id, name))
-    except:
+    except Exception:
         print("Nothing")
     session.close()
