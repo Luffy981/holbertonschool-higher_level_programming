705a0d8803526debeaedfefe0324c3c9e796f850
luffy981 <jhonsmith981@hotmail.com>
2021-12-18 12:20:51 -0500

    All states via SQLAlchemy|

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,6 +6,9 @@ from sys import argv
 
 
 if __name__ == "__main__":
+    """
+    Conect database and quering
+    """
     sql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(sql.format(argv[1],
                                       argv[2], argv[3]), pool_pre_ping=True)
