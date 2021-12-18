2b993ba4bca397960de33a167f21e1de7da01307
luffy981 <jhonsmith981@hotmail.com>
2021-12-18 11:04:37 -0500

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,6 +12,7 @@ def connectDB():
                                         user=argv[1], password=argv[2],
                                         db=argv[3], charset="utf8")
     except Exception:
+        print("Can't connect to database")
         return 0
     cur = db_connection.cursor()
     sql = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
