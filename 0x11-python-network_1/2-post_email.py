7b479dabf50ecc73e8e38c8f6ebe1ff9b3f3b5a1
luffy981 <jhonsmith981@hotmail.com>
2022-01-09 17:01:00 -0500

    POST method

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,11 +7,11 @@ jojo
 
 if "__name__" == "__main__":
     from sys import argv
-    from urllib.request import urlopen, Request
+    import urllib.request
     import urllib.parse
     values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')
-    req = Request(argv[1], data)
-    with urlopen(req) as response:
+    req = urllib.request.Request(argv[1], data)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
