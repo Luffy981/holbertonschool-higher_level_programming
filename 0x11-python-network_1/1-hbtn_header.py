07f7f74df871de9a78ff9e4b8978a46d2eb0549d
luffy981 <jhonsmith981@hotmail.com>
2022-01-09 15:32:52 -0500

    request variable

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# sends a request to the URL and displays the value of the X-Request-Id
+
+from urllib.request import urlopen
+from sys import argv
+
+
+if __name__=="__main__":
+    with urlopen(argv[1]) as response:
+        headers_like_dict = dict(response.getheaders())
+        print(headers_like_dict.get('x-Request-Id'))
