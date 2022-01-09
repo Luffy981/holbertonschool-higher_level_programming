364ab1429faecbf809e5d22dadda8f914bb0ac9a
luffy981 <jhonsmith981@hotmail.com>
2022-01-09 15:41:40 -0500

    request fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 # Python script that fetches https://intranet.hbtn.io/status
 
+from urllib.request import urlopen
 
-if __name__ == "__main__":
-    from urllib.request import urlopen
-    actions = [
-            ("type", lambda html: type(html)),
-            ("content", lambda html: html),
-            ("utf8 content", lambda html: html.decode()),
-    ]
-    with urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        for header, fun in actions:
-            print("\t- {}: {}".format(header, fun(html)))
+actions = [
+        ("type", lambda html: type(html)),
+        ("content", lambda html: html),
+        ("utf8 content", lambda html: html.decode()),
+]
+
+with urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:")
+    for header, fun in actions:
+        print("\t- {}: {}".format(header, fun(html)))
