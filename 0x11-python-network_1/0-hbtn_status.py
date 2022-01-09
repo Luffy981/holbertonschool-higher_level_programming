675aeebb748739e0d0e6ad98a5c2208675c9304a
luffy981 <jhonsmith981@hotmail.com>
2022-01-09 15:12:53 -0500

    fetch page

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # Python script that fetches https://intranet.hbtn.io/status
+#
 from urllib.request import urlopen
 
 actions = [
         ("type", lambda html: type(html)),
         ("content", lambda html: html),
         ("utf8 content", lambda html: html.decode()),
-    ]
+]
 
 with urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
