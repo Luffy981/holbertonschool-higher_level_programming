e1bbb2075e5b840ff7b9602294dd73412287cd38
luffy981 <jhonsmith981@hotmail.com>
2022-01-09 16:57:53 -0500

    POST method

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email, sends a POST request
+ displays the value of the X-Request-Id
+ variable found in the header of the response
 """
-
-
-if "__name__" == "__main__":
+if __name__ == "__main__":
+    import urllib.request as request
+    import urllib.parse as parse
     from sys import argv
-    from urllib.request import urlopen, Request
-    import urllib.parse
     values = {'email': argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')
-    req = Request(argv[1], data)
-    with urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+    data = parse.urlencode(values).encode('utf-8')
+    r = request.Request(argv[1], data)
+    with request.urlopen(r) as r:
+        print(r.read().decode('utf-8'))
