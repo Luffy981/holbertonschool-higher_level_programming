658c9d4b72b134936497676c387f70fc6f76f050
luffy981 <jhonsmith981@hotmail.com>
2022-01-09 20:44:21 -0500

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
+# script that fetches https://intranet.hbtn.io/status
 from urllib.request import urlopen
 
-actions = [
-        ("type", lambda html: type(html)),
-        ("content", lambda html: html),
-        ("utf8 content", lambda html: html.decode()),
-    ]
-
-with urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    for header, fun in actions:
-        print("\t- {}: {}".format(header, fun(html)))
+if __name__ == "__main__":
+    with urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(hml.decode('utf-8'))
