44f21fd0443946a49e54f226bf889302e1cad455
luffy981 <jhonsmith981@hotmail.com>
2022-02-01 22:03:30 -0500

    read files with NDJS

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs')
-const filename = process.argv[2]
+const fs = require('fs');
+const filename = process.argv[2];
 fs.readFile(filename, 'utf8', (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  console.log(data)
-})
+  console.log(data);
+});
