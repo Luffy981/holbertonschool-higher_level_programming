725bda82a82a96857ee9b4241a78d757783c161a
luffy981 <jhonsmith981@hotmail.com>
2022-02-01 22:25:12 -0500

    write to files with NodeJs

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const filename = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(filename, content, err => {
+fs.writeFile(filename, content, 'utf8', err => {
   if (err) {
     console.error(err);
   }
