fe72f71beef829d29ecdcd03522a7fea6e859214
luffy <jhonsmith981@hotmail.com>
2022-02-10 10:34:50 -0500

    lists the title for all movies from URL

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,10 +1,10 @@
-$.get("https://swapi-api.hbtn.io/api/films/?format=json", function(data, status){
-  for (const [key, value] of Object.entries(data)){
-    if (key === 'results'){
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data, status) {
+  for (const [key, value] of Object.entries(data)) {
+    if (key === 'results') {
       value.forEach(element => {
-        for (const [ky, val] of Object.entries(element)){
-          if (ky === "title"){
-            $("#list_movies").append(`<li>${val}</li>`)
+        for (const [ky, val] of Object.entries(element)) {
+          if (ky === 'title') {
+            $('#list_movies').append(`<li>${val}</li>`);
           }
         }
       });
