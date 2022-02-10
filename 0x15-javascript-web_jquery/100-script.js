e888c64471b5c05d3bf9adef8b1d02950c41a182
luffy <jhonsmith981@hotmail.com>
2022-02-10 12:24:43 -0500

    text color of the <header> element to red (#FF0000) imported from the <head>

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,5 +1,5 @@
-(function(){
-  document.addEventListener('DOMContentLoaded', function(){
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('header').style.color = '#FF0000';
-  })
+  });
 })();
